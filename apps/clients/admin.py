0f997ebbbe414688d3c6208aac22e31649b2f74c
Abdul Shukoor <abdulshukoorpk@gmail.com>
2015-04-14 15:56:16 +0530

    admin registered

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Client, MedicalHistory, ClientFeed
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(MedicalHistory)
+admin.site.register(ClientFeed)
--- apps/trainers/models.py
+++ apps/trainers/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.utils import timezone
 
 from apps.account.base import UserProfileBase
-# from apps.clients.models import Client
+from apps.clients.models import Client
 # Create your models here.
 
 
@@ -13,25 +13,3 @@ class Trainer(UserProfileBase):
     month_rate_3 = models.PositiveSmallIntegerField(max_length=4)
     month_rate_6 = models.PositiveSmallIntegerField(max_length=4)
     month_rate_12 = models.PositiveSmallIntegerField(max_length=4)
-
-
-# class Task(models.Model):
-#     # client = models.ForeignKey(Client, related_name='tasks')
-#     date = models.DateField(default=timezone.now)
-#     excercise_type = models.CharField(max_length=40)
-#     count = models.PositiveSmallIntegerField(default=0)
-#     comment = models.CharField(max_length=256)
-#     media = models.ForeignKey(Media, related_name='tasks')
-
-
-# class Routine(Task):
-#     pass
-
-#     # class Meta:
-#     #     fields = (
-#     #         'client', 'date', 'excercise_type', 'count', 'comment', 'media'
-#     #     )
-
-# class Media(models.Model):
-#     video = models.FileField(upload_to='videos')
-#     image = models.FileField(upload_to='images')
