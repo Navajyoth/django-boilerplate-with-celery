6832366d529ee5122a268272ca9093f48de54d18
Abdul Shukoor <abdulshukoorpk@gmail.com>
2015-04-14 16:55:43 +0530

    fixing conflicts

--- apps/clients/urls.py
+++ apps/clients/urls.py
@@ -1,15 +1,7 @@
 from django.conf.urls import patterns, url
-<<<<<<< HEAD
 
 from apps.clients import views 
 
 urlpatterns= patterns('',
     url(r'^$', views.home),
     )
-=======
-from apps.clients import views
-
-urlpatterns = patterns('',
-                       url(r'^$', views.home),
-                       )
->>>>>>> origin
--- apps/clients/views.py
+++ apps/clients/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-<<<<<<< HEAD
+
 from apps.clients.models Clients, MedicalHistory, ClientFeed
 
 def home(request):
     return HttpResponse('site under construction')
-
-=======
-
-def home(request):
-    return HttpResponse('Site under construction')
->>>>>>> origin
