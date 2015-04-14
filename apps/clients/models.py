f59e7bc7e196a2cae344b3be49785e4dc5efb3d8
Navajyoth <navajyothms1989@gmail.com>
2015-04-14 14:51:34 +0530

    added model for clients

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from appps.account.base import UserProfileBase
+from apps.trainers.models import Trainer
 
-# Create your models here.
+
+class Client(UserProfileBase):
+    trainer = models.ForeignKey(Trainer, related_name='clients')
+    height = models.CharField(max_length=7)
+	weight = models.CharField(max_length=7)
+	bloodtype = models.CharField(max_length=5)
+	occupation = models.CharField(max_length=30)
+	goal_weight = models.CharField(max_length=7)
+	goal_achieved = models.BooleanField(default=False)
+	wake_up_time = models.DateTimeField(default=timezone.now)
+	bed_time = models.DateTimeField(default=timezone.now)
+	sit_more_than_30hrs/week = models.BooleanField(default=False)
+	constant_back_pain = models.BooleanField(default=False)
+	currently_pregnant = models.BooleanField(default=False)
+	weekly_workout_schedule = models.CharField(max_length=25)
+
+
+class MedicalHistory(models.Model):
+    client = models.ForeignKey(Client, related_name ='medical histories')
+    time_of_injury = models.DateTimeField(default=timezone.now, verbose='Hospital Injury')
+    comments = models.CharField(max_length=512)
+
+
+class ClientFeed(models.Model):
+	message = models.CharField(max_length=512)
+	datetime = models.DateTimeField(default=timezone.now)
+	client = models.ForeignKey(Client, related_name='Client Feed')
+
+
