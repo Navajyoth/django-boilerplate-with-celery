4caf38e1c1bb479508591a2086a9d55492e1234e
Navajyoth <navajyothms1989@gmail.com>
2015-04-14 15:08:47 +0530

    model changed

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -6,28 +6,28 @@ from apps.trainers.models import Trainer
 class Client(UserProfileBase):
     trainer = models.ForeignKey(Trainer, related_name='clients')
     height = models.CharField(max_length=7)
-	weight = models.CharField(max_length=7)
-	bloodtype = models.CharField(max_length=5)
-	occupation = models.CharField(max_length=30)
-	goal_weight = models.CharField(max_length=7)
-	goal_achieved = models.BooleanField(default=False)
-	wake_up_time = models.DateTimeField(default=timezone.now)
-	bed_time = models.DateTimeField(default=timezone.now)
-	sit_more_than_30hrs/week = models.BooleanField(default=False)
-	constant_back_pain = models.BooleanField(default=False)
-	currently_pregnant = models.BooleanField(default=False)
-	weekly_workout_schedule = models.CharField(max_length=25)
+    weight = models.CharField(max_length=7)
+    bloodtype = models.CharField(max_length=5)
+    occupation = models.CharField(max_length=30)
+    goal_weight = models.CharField(max_length=7)
+    goal_achieved = models.BooleanField(default=False)
+    wake_up_time = models.DateTimeField(default=timezone.now)
+    bed_time = models.DateTimeField(default=timezone.now)
+    sit_more_than_30hrs/week = models.BooleanField(default=False)
+    constant_back_pain = models.BooleanField(default=False)
+    currently_pregnant = models.BooleanField(default=False)
+    weekly_workout_schedule = models.CharField(max_length=25)
 
 
 class MedicalHistory(models.Model):
-    client = models.ForeignKey(Client, related_name ='medical histories')
-    time_of_injury = models.DateTimeField(default=timezone.now, verbose='Hospital Injury')
+    client = models.ForeignKey(Client, related_name='medical histories')
+    time_of_injury = models.DateTimeField(
+        default=timezone.now, verbose='Hospital Injury'
+    )
     comments = models.CharField(max_length=512)
 
 
 class ClientFeed(models.Model):
-	message = models.CharField(max_length=512)
-	datetime = models.DateTimeField(default=timezone.now)
-	client = models.ForeignKey(Client, related_name='Client Feed')
-
-
+    message = models.CharField(max_length=512)
+    datetime = models.DateTimeField(default=timezone.now)
+client = models.ForeignKey(Client, related_name='Client Feed')
